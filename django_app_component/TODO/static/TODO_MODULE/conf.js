b003bc7286b65f5b8e2d6d164327d4c6317a0cb7
ShunTabi <s-kobata.ju@outlook.com>
2021-10-23 01:31:58 +0900

    commit

--- django_app_component/TODO/static/TODO_MODULE/conf.js
+++ django_app_component/TODO/static/TODO_MODULE/conf.js
@@ -1,4 +1,5 @@
 "use strict";
 const sql_limit = 9;
+// const url = "http://localhost:8080/"
 const url = "http://192.168.10.100:8080/"
 export { sql_limit, url }
---
+++ django_app_component_localhost.bat
@@ -0,0 +1,3 @@
+pushd E:\05.DEV_2\django_app_component
+start http://localhost:8080/TODO/#/TODO_HEADER_TOP/1
+python manage.py runserver localhost:8080
