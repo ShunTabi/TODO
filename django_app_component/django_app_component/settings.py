1b2a64534cd4907040514ca2a334cea0123ec1c2
ShunTabi <s-kobata.ju@outlook.com>
2021-10-23 01:35:16 +0900

    commit

--- django_app_component/django_app_component/settings.py
+++ django_app_component/django_app_component/settings.py
@@ -8,7 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&syaw@p!hq4c*d00hp^z(5l0bxd=e4%clilwdk^_b%m31i=pcj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
